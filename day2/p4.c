e9e70f36513d7221c181e58798be9940dda2989c
Harsha2024 <harshaachar55@gmail.com>
2025-03-07 11:14:09 +0530

    harsshhhaaaa

--- day2/p4.c
+++ day2/p4.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    int a = 13, b = 2, c = -8;
-    if(a <= b & ( ++b == --c || a > c ++))
+    int m = 13, n = 2, c = -8;
+    if(a <= n & ( ++n == --c || m > c ++))
         puts("Tadiamdamol");
     else
         puts("Pushpaaaaaagiri");
 
-    printf("%d %d %d \n", a, b, c);
+    printf("%d %d %d \n", m, n, c);
 
 
 
