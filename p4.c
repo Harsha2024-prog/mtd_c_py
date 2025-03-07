0731c1d0bd261fef3594ae3d39ecd7c241930f66
Harsha2024 <harshaachar55@gmail.com>
2025-03-07 10:39:31 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a = 13, b = 2, c = -8;
+    if(a <= b & ++b == --c || a > c ++)
+        puts("Tadiamdamol");
+    else
+        puts("Pushpagiri");
+
+    printf("%d %d %d \n", a, b, c);
+
+
+
+}
