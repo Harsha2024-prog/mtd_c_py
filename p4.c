199b77aa46cd7a7fdd091fc62ad6386bb11be84b
Harsha2024 <harshaachar55@gmail.com>
2025-03-07 10:43:46 +0530

    Update p4.c

--- p4.c
+++ p4.c
@@ -2,7 +2,7 @@
 
 int main(){
     int a = 13, b = 2, c = -8;
-    if(a <= b & ++b == --c || a > c ++)
+    if(a <= b & ( ++b == --c || a > c ++))
         puts("Tadiamdamol");
     else
         puts("Pushpagiri");
